6e11f24508862b3cb10aa0c74022806e0deb233c
Gabriel Kanashiro <gabriel.kanashiro@avenuecode.com>
2022-05-05 00:16:06 -0300

    terceira aula

---
+++ server/README.md
@@ -0,0 +1,9 @@
+# Aula 3
+* Principios de backend
+* Metodos HTTP
+* Status code
+* Como ligar com o banco de dados usando o Prisma
+* Conceitos do SOLID
+* Inversão de dependecias
+* Testes unitários com Jest
+* Envios de email com node
---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapters.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "c6276bb2de2088",
+    pass: "48e524f59f1766"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to:  'Gabriel Kanashiro <boatentativa@google.com>',
+      subject,
+      html: body
+    });
+  }
+}
